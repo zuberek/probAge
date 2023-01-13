d324bf24242bec0e13a602db604db898be37c665
Jan Dabrowski <s1768506@ed.ac.uk>
2023-01-13 11:25:35 +0000

    Update naming

---
+++ src/general_imports.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
+from multiprocess import Pool
+import anndata as ad
+sns.set_theme(style='ticks')
+
+# import custom code
+import sys
+sys.path.append("..")   # fix to import modules from root
+from src.amdata import amdata
+from src.utils import plot
+from src import preprocess_func
---
+++ src/preprocess_func.py
@@ -0,0 +1,8 @@
+
+def convert_mvalues_to_betavalues(amdata):
+    2**amdata.X/(2**amdata.X + 1)
+
+def merge_meta(amdata, meta):
+    amdata.obs['pos'] = range(amdata.n_obs) # to have numerical index stored as well
+    amdata.var['pos'] = range(amdata.n_vars)
+    amdata.var[['name', 'age', 'sex']] = meta[['Sample_Name', 'age', 'sex']]
