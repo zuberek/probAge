241e6745f8ebc2f9a93acca4cf7e13d5339edb26
Jan Dabrowski <s1768506@ed.ac.uk>
2023-01-14 12:25:01 +0000

    Efficiently compute r2

--- scripts/amdata_process.py
+++ scripts/amdata_process.py
@@ -5,7 +5,8 @@ from src.general_imports import *
 
 from sklearn.feature_selection import r_regression
 
-INPUT_PATH = '../exports/wave3.h5ad'
+INPUT_PATH = '../exports/wave3_raw.h5ad'
+INPUT_PATH = '../exports/wave3_meta.h5ad'
 META_DIR_PATH = '/afs/inf.ed.ac.uk/user/s17/s1768506/disk/methylation/genscot_meta/wave3'
 
 print('Loading the dataset (it might take a while)...')
@@ -18,18 +19,33 @@ clock_results = pd.read_csv(f'{META_DIR_PATH}/DNAmAge_output.csv', index_col='Ba
 clock_sites = pd.read_csv('../resources/AllGS_CpGs.csv', index_col='CpGs')
 
 print('Dropping NaNs...')
-preprocess_func.drop_nans(amdata)
+amdata = preprocess_func.drop_nans(amdata)
 
 print('Merging the metadata...')
 preprocess_func.merge_meta(amdata, 
                     meta, pheno, survival, clock_results, clock_sites)
 
 print('Converting to beta values (it might take a while)...')
-preprocess_func.convert_mvalues_to_betavalues(amdata)
+amdata.X = preprocess_func.convert_mvalues_to_betavalues(amdata)
 
-site_index = amdata.obs.index[0]
-site_data = amdata[site_index].X.flatten()
-site_target = amdata.var.age
-r2_score(site_data, site_target)
-r_regression(amdata_T[:500], site_target)
-amdata_T = amdata.copy().transpose()
+print('Computing the r2 (it might take a while)...')
+def r2(site_index):
+    return r_regression(amdata[site_index].X.T, amdata.var.age)[0]**2
+r2('cg16867657')
+
+
+with Pool() as p:
+    output = list(tqdm(p.imap(
+            func=r2, 
+            iterable=amdata.obs.index,
+            chunksize=len(amdata.obs.index)//cpu_count()
+            ),
+        total=len(amdata.obs.index)))
+# [01:47<00:00, 7171.79it/s] (16 CPUs)
+
+amdata.obs['r2'] = output
+amdata.obs.sort_values('r2')
+
+
+amdata.write_h5ad('../exports/wave3_meta.h5ad')
+
--- src/preprocess_func.py
+++ src/preprocess_func.py
@@ -5,17 +5,17 @@ def drop_nans(amdata):
     nans = np.isnan(amdata.X).sum(axis=1).astype('bool')
     print(f'There were {nans.sum()} NaNs dropped')
     # Use the ~ character to flip the boolean
-    amdata = amdata[~nans]
+    return amdata[~nans]
 
 def convert_mvalues_to_betavalues(amdata):
-    amdata.X = 2**amdata.X/(2**amdata.X + 1)
+    return 2**amdata.X/(2**amdata.X + 1)
 
 def merge_meta(amdata, meta, pheno, survival, clock_results, clock_sites): 
 
     # Add basic metadata
     ####################
-    amdata.obs['pos'] = range(amdata.n_obs) # to have numerical index stored as well
     amdata.var['pos'] = range(amdata.n_vars)
+    amdata.obs['pos'] = range(amdata.n_obs) # to have numerical index stored as well
     amdata.var[['name', 'age', 'sex']] = meta[['Sample_Name', 'age', 'sex']]
 
     # Add phenotype data and survival data
@@ -37,7 +37,6 @@ def merge_meta(amdata, meta, pheno, survival, clock_results, clock_sites):
     clock_columns = ['Hannum','Horvath','Grim','Pheno']
     amdata.var[clock_columns] = clock_results[clock_columns]
 
-
     # Mark sites that were already used in different clocks
     ####################
     # remove sites that are used in clocks but not in generation scotland
@@ -46,4 +45,8 @@ def merge_meta(amdata, meta, pheno, survival, clock_results, clock_sites):
     for group_name, group_data in clock_sites.groupby('Clock'):
         amdata.obs[group_name] = False
         amdata.obs.loc[group_data.index, group_name] = True
+
+
+
+
  
