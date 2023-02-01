ae568e979c7bb85be177f36939c605954461bc44
Jan Dabrowski <s1768506@ed.ac.uk>
2023-02-01 11:30:06 +0000

    Run full batch correction for hannum

--- scripts/quick_model.py
+++ scripts/quick_model.py
@@ -13,49 +13,58 @@ logger.propagate = False
 logger.setLevel(logging.ERROR)
 
 
-wave3 = amdata_src.AnnMethylData('../exports/wave3_linear.h5ad', backed='r')
+wave3 = amdata_src.AnnMethylData('../exports/wave3_linear.h5ad')
 
 DATA_PATH = '../exports/hannum.h5ad'
 
 amdata = amdata_src.AnnMethylData(DATA_PATH)
 amdata= ad.read_h5ad(DATA_PATH)
 
+# Add ground truth from wave3
 params = ['mean_slope','mean_inter','var_slope','var_inter']
 intersection = wave3.obs.index.intersection(amdata.obs.index)
 amdata = amdata[intersection]
 wave3 = wave3[intersection]
 amdata.obs[params] = wave3.obs[params]
+amdata = amdata_src.AnnMethylData(amdata)
 
-# amdata = amdata[amdata.obs.sort_values('r2', ascending=False).index[:N_SITES]]
-
+# Refit to quickly check for differences
+true_params = [f'true_{param}' for param in params]
 maps = modelling.drift_sites(amdata, return_MAP=True, return_trace=False, show_progress=True)['map']
-amdata.sites[params] = maps[params]
+for param in params:
+    amdata.sites[f'true_{param}'] = maps[param]
+
+axs = plot.tab(params, 'Hannum - Wave 3 comparison', ncols=2, row_size=5, col_size=8)
+for i, param in enumerate(params):
+    sns.scatterplot(data=amdata.sites, x=param, y=f'true_{param}', ax=axs[i])
 
 amdata.write_h5ad(DATA_PATH)
 
+# Calculate predictions without any correction
 ab_maps = modelling.person_model(amdata, return_MAP=True, return_trace=False, show_progress=True)['map']
+sns.histplot(x=ab_maps['acc'], y=ab_maps['bias'], cbar=True, ax=plot.row('Before'))
 
-amdata.var['acc'] = ab_maps['acc']
-amdata.var['bias'] = ab_maps['bias']
-
-sns.histplot(data=amdata.var, x='acc', y='bias', cbar=True, ax=plot.row('Wave 1: After'))
-
-maps = modelling.dset_offsets(amdata, return_MAP=True, return_trace=False, show_progress=True)['map']
+# Infer the offsets
+maps = modelling.site_offsets(amdata, return_MAP=True, return_trace=False, show_progress=True)['map']
 amdata.obs['offset'] = maps['offset']
-
-amdata.obs.offset.hist()
-
+sns.histplot(amdata.obs.offset)
 amdata.obs.sort_values('offset')
 
-offset = amdata.obs.offset
+# Plot the data correction
+ax = plot.row('Top shifted down')
+sns.scatterplot(x=wave3.var.age, y=wave3['cg07553761'].X.flatten(), label='wave3',ax=ax)
+sns.scatterplot(x=amdata.var.age, y=amdata['cg07553761'].X.flatten(), label='hannum', ax=ax)
+sns.scatterplot(x=amdata.var.age, y=amdata['cg07553761'].X.flatten()-amdata['cg07553761'].obs.offset.values[0], label='hannum (corr)', ax=ax)
+
+ax = plot.row('Top shifted up')
+sns.scatterplot(x=wave3.var.age, y=wave3['cg00048759'].X.flatten(), label='wave3',ax=ax)
+sns.scatterplot(x=amdata.var.age, y=amdata['cg00048759'].X.flatten(), label='hannum', ax=ax)
+sns.scatterplot(x=amdata.var.age, y=amdata['cg00048759'].X.flatten()-amdata['cg00048759'].obs.offset.values[0], label='hannum (corr)', ax=ax)
+
+# Apply the offset and refit acceleration and bias
 offset = np.broadcast_to(amdata.obs.offset, shape=(amdata.n_participants, amdata.n_sites)).T
-amdata.X = amdata.X + offset
-ax = plot.row('Top shifted: Before')
-sns.scatterplot(x=wave3.var.age, y=wave3['cg11142333'].X.flatten(), label='wave3',ax=ax)
-sns.scatterplot(x=amdata.var.age, y=amdata['cg11142333'].X.flatten(), label='hannum', ax=ax)
-sns.scatterplot(x=amdata.var.age, y=amdata['cg11142333'].X.flatten()-amdata['cg11142333'].obs.offset.values[0], label='hannum (corr)', ax=ax)
+amdata.X = amdata.X - offset
+ab_maps = modelling.person_model(amdata, return_MAP=True, return_trace=False, show_progress=True)['map']
+sns.histplot(x=ab_maps['acc'], y=ab_maps['bias'], cbar=True, ax=plot.row('After'))
 
-wave3.plot.site('cg04875128')
-sns.scatterplot(x=amdata.participants.age, y=amdata['cg04875128'].X.flatten())
 
-sns.histplot(offset[:,0])
