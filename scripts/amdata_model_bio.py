d23fd0a5f2765e0fe8a3f962656f2438e8d5f6ba
elc08 <lactovit@hotmail.com>
2023-02-07 16:16:42 +0000

    amdata model bio update

--- scripts/amdata_model_bio.py
+++ scripts/amdata_model_bio.py
@@ -1,6 +1,7 @@
 # %%
 %load_ext autoreload 
 %autoreload 2
+
 import sys
 sys.path.append("..")   # fix to import modules from root
 from src.general_imports import *
@@ -15,16 +16,13 @@ logger = logging.getLogger('pymc')
 logger.propagate = False
 logger.setLevel(logging.ERROR)
 
-N_SITES = 60
+N_SITES = 7
 N_PARTS = False
-N_CORES = 60
+N_CORES = 7
 
 amdata = amdata_src.AnnMethylData('../exports/wave3_meta.h5ad', backed='r')
 amdata = amdata[amdata.obs.sort_values('r2', ascending=False).index[:N_SITES]].to_memory()
 amdata = amdata_src.AnnMethylData(amdata)
-
-# %%
-%%time
 
 with Pool(N_CORES, maxtasksperchild=1) as p:
     results = list(tqdm(
@@ -34,19 +32,17 @@ with Pool(N_CORES, maxtasksperchild=1) as p:
                 chunksize=1
                 ), 
             total=amdata.n_obs))
-# %%
-modelling_bio.fit_and_compare(amdata[:10], show_progress=True)
 
 print('Exporting site model results')
 fits, comparisons = modelling_bio.comparison_postprocess(results, amdata)
-comparisons.to_csv('../exports/comparison_bio.csv')
-fits.to_csv('../exports/fits_bio.csv')
+# comparisons.to_csv('../exports/comparison_bio.csv')
+# fits.to_csv('../exports/fits_bio.csv')
 
 full_comparisons_plot = (
     modelling_bio.full_comparison_plot(comparisons))
-full_comparisons_plot.figure.savefig('../results/full_comparison.png')
+# full_comparisons_plot.figure.savefig('../results/full_comparison.png')
 
-amdata.write_h5ad('../exports/wave3_bio.h5ad')
+# amdata.write_h5ad('../exports/wave3_bio.h5ad')
 
 # drop saturating sites
 amdata = amdata[amdata.obs['saturating'] == False]
@@ -56,8 +52,6 @@ print('Acceleration and bias model fitting')
 if N_PARTS is not False:
     amdata = amdata[:, :N_PARTS]
 
-
-
 # create amdata chunks to vectorize acc and bias over participants
 chunk_size = 10
 n_participants = amdata.shape[1]
@@ -65,13 +59,12 @@ amdata_chunks = []
 for i in range(0, n_participants, chunk_size):
     amdata_chunks.append(amdata[:,i:i+chunk_size])
 
-with Pool(N_CORES, maxtasksperchild=1) as p:
-    results = list(tqdm(
-            iterable= p.imap(
-                func=modelling_bio.person_model_reparam,
-                iterable=amdata_chunks, 
-                ), 
-            total=len(amdata_chunks)))
+results = []
+for chunk in tqdm(amdata_chunks):
+    results.append(
+        modelling_bio.person_model(chunk
+        )
+    )
 
 traces = results[0]['trace']
 modelling_bio.make_clean_trace(traces)
@@ -84,6 +77,7 @@ to_save_acc = [f'acc_{col}' for col in to_save]
 to_save_bias = [f'bias_{col}' for col in to_save]
 amdata.var[to_save_acc] = az.summary(traces, var_names=['acc'])[to_save].values
 amdata.var[to_save_bias] = az.summary(traces, var_names=['bias'])[to_save].values
+amdata.var[to_save_acc] = np.log2(amdata.var[to_save_acc])
 
 amdata.write_h5ad('../exports/wave3_acc_bio.h5ad')
 
