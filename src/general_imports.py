1959ab83f855b4d47f29e38bd27b36b87149a999
elc08 <lactovit@hotmail.com>
2023-02-09 13:41:28 +0000

    include partial

--- src/general_imports.py
+++ src/general_imports.py
@@ -7,6 +7,8 @@ from multiprocessing import Pool
 from multiprocessing import cpu_count
 import anndata as ad
 import json
+
+from functools import partial
 # sns.set_theme(style='ticks')
 
 # import custom code
