76a588b3f129356466deb04015f50dd116f1630c
Jan Dabrowski <s1768506@ed.ac.uk>
2023-02-11 12:50:53 +0000

    Move plot scripts to a dedicated folder

--- plots/plot_n_sites.py
+++ plots/plot_n_sites.py
@@ -8,8 +8,13 @@ from src import modelling_bio
 
 N_SITES = 20
 
-site_indexes = amdata.obs.sort_values('r2').tail(N_SITES).index
-
+site_indexes = amdata[~amdata.obs.saturating].obs.sort_values('r2', ascending=False).head(N_SITES).index
+site_indexes = amdata.obs.sort_values('der', ascending=False).tail(N_SITES).index
+site_indexes = amdata.obs[amdata.obs.saturating].sort_values('r2', ascending=False).head(N_SITES).index
+site_indexes = amdata.obs.sort_values('var_init', ascending=False).head(N_SITES).index
+site_indexes = amdata.obs[amdata.obs.var_init>1].sort_values('var_init', ascending=False).tail(N_SITES).index
+site_indexes = amdata.obs[~amdata.obs.saturating * amdata.obs.saturating_old].tail(N_SITES).index
+site_indexes = amdata.obs[amdata.obs.saturating].sort_values('r2', ascending=False).head(N_SITES).index
 
 axs = plot.tab(site_indexes, ncols=5, row_size=5)
 for i, site_index in enumerate(site_indexes): 
