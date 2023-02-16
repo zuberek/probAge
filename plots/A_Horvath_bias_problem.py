3e3322b52e6d2be8f6d17c89bdf7ae5126e1bd53
Jan Dabrowski <s1768506@ed.ac.uk>
2023-02-16 19:20:08 +0000

    Upload new plots

--- plots/A_Horvath_bias_problem.py
+++ plots/A_Horvath_bias_problem.py
@@ -15,41 +15,129 @@ horvath_coef = pd.read_csv('../resources/Horvath_coef.csv', index_col=0)
 intersection = amdata.obs.index.intersection(horvath_coef.index)
 amdata_horvath = amdata[intersection].to_memory()
 
-#%% compute accelerations
-global_offsets = np.array([-0.1, -0.05, 0, 0.05, 0.1])
+horvath_data = pd.read_csv('../exports/GS-P2-Wave3-Phe_hyperHypo.csv', index_col=0)
+horvath_data.columns[150:200]
+age = horvath_data['age'].values
+horvath_data = horvath_data.iloc[:,3:]
+ages = np.broadcast_to(age, (horvath_data.shape[1], horvath_data.shape[0])).T
+horvath_data = horvath_data - ages
 
-mod=LinearRegression().fit(X=amdata_horvath.X.T, y=amdata_horvath.var.age)
-amdata_horvath.var['new_Horvath']=mod.predict(amdata_horvath.X.T)-amdata.var.age
-sns.scatterplot(data=amdata_horvath.var, x='Horvath', y='new_Horvath')
+horvath_data['Horvath'].hist()
+
+# %%
+clocks = ['Horvath', 'SkinClock', 'Hannum']
+# clocks = ['Hannum','SkinClock','Horvath']
+offsets=[0.01,0.02,0.03,0.04,0.05,0.15]
+offsets=[0.05,0.15]
+all_offsets = sorted([-offset for offset in offsets]) + [0] + offsets
+
+all_clock_data = pd.DataFrame()
+for clock in clocks:
+    columns =   [f'{int(offset*100)}%MinusAbs-{clock}' for offset in sorted(offsets, reverse=True)] + \
+            [clock] + \
+            [f'{int(offset*100)}%PlusAbs-{clock}' for offset in offsets] 
+    clock_data = horvath_data[columns].copy()
+    clock_data.columns = all_offsets
+    clock_data['clock'] = clock
+    all_clock_data = pd.concat([all_clock_data, clock_data])
+
+
+df = all_clock_data.melt(id_vars='clock', var_name='offset', value_name='age acceleration')
+# sns.boxplot(data=df, x='clock', y='age acceleration', hue='offset',  showfliers=False)
+# %%
+ax=plot.row('')
+ax.axhline(y=0, dashes=[5,3], color='tab:grey')
+sns.boxplot(data=df, x='offset', y='age acceleration', hue='clock',  showfliers=False, ax=ax)
+sns.despine()
+
+plot.save(ax, '1_clock_global_offset_problem', format=['png','svg'])
+# %%
 
 accs =[]
-horvaths = []
 
-for offset in tqdm(global_offsets):
+for offset in tqdm(all_offsets):
     amdata_bio_offset = amdata_bio.copy()
-    amdata_horvath_offset = amdata_horvath.copy()
     amdata_bio_offset.X = amdata_bio.X+offset
-    amdata_horvath_offset.X = amdata_horvath.X+offset
     maps = modelling_bio.person_model(amdata_bio_offset, return_trace=False, return_MAP=True, show_progress=False, cores=4)['map']
     accs.append(maps['acc'])
-    horvaths.append((mod.predict(amdata_horvath_offset.X.T)-amdata_horvath.var.age).values)
 
-#%% transform output
-accs_df=pd.DataFrame(accs, index=np.round(global_offsets,2)).reset_index().melt(id_vars=['index'], var_name='offset').assign(type='acc')
-horvaths_df=pd.DataFrame(horvaths, index=np.round(global_offsets,2)).reset_index().melt(id_vars=['index'], var_name='offset').assign(type='horvath')
-
-accs_df['std'] = accs_df.groupby('index')['value'].transform('std')
-accs_df['normalised_value'] = accs_df.value/accs_df['std']
-horvaths_df['std'] = horvaths_df.groupby('index')['value'].transform('std')
-horvaths_df['normalised_value'] = horvaths_df.value/horvaths_df['std']
-
+np.array(accs).shape
+accs_df=pd.DataFrame(accs, index=np.round(all_offsets,2)).T.melt(var_name='offset', value_name='acc').assign(type='acc')
+pd.DataFrame(accs, index=np.round(all_offsets,2)).T
+#%%
+horvaths_df = all_clock_data[all_clock_data.clock=='Horvath'].copy()
+horvaths_df=horvaths_df[all_offsets].melt(var_name='offset', value_name='acc').assign(type='horvath')
+accs_df['std'] = accs_df.groupby('offset')['acc'].transform('std')
+accs_df['normalised_acc'] = accs_df.acc/accs_df['std']
+horvaths_df['std'] = horvaths_df.groupby('offset')['acc'].transform('std')
+horvaths_df['normalised_acc'] = horvaths_df.acc/horvaths_df['std']
 df = pd.concat((accs_df, horvaths_df), axis=0)
-df = df.drop(['offset'], axis=1).rename(columns={'index': 'offset'})
 
 #%% plot
 ax=sns.boxplot(data=df, x='offset', 
-                y='normalised_value', hue='type', showfliers=False)
+                y='normalised_acc', hue='type', showfliers=False)
+ax.axhline(y=0, dashes=[5,3], color='tab:grey')
+sns.despine()
 
 #%% save
-plot.save(ax, 'A_Horvath_bias_problem', format='svg')
-plot.save(ax, 'A_Horvath_bias_problem', format='png')
+plot.save(ax, 'A_clock_global_offset_problem_horvathVSacc', format=['png','svg'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #%% compute accelerations
+# global_offsets = np.array([-0.1, -0.05, 0, 0.05, 0.1])
+
+# mod=LinearRegression().fit(X=amdata_horvath.X.T, y=amdata_horvath.var.age)
+# amdata_horvath.var['new_Horvath']=mod.predict(amdata_horvath.X.T)-amdata.var.age
+# sns.scatterplot(data=amdata_horvath.var, x='Horvath', y='new_Horvath')
+
+# accs =[]
+# horvaths = []
+
+# for offset in tqdm(global_offsets):
+#     amdata_bio_offset = amdata_bio.copy()
+#     amdata_horvath_offset = amdata_horvath.copy()
+#     amdata_bio_offset.X = amdata_bio.X+offset
+#     amdata_horvath_offset.X = amdata_horvath.X+offset
+#     maps = modelling_bio.person_model(amdata_bio_offset, return_trace=False, return_MAP=True, show_progress=False, cores=4)['map']
+#     accs.append(maps['acc'])
+#     horvaths.append((mod.predict(amdata_horvath_offset.X.T)-amdata_horvath.var.age).values)
+
+
+# #%% transform output
+# accs_df=pd.DataFrame(accs, index=np.round(global_offsets,2)).reset_index().melt(id_vars=['index'], var_name='offset').assign(type='acc')
+# horvaths_df=pd.DataFrame(horvaths, index=np.round(global_offsets,2)).reset_index().melt(id_vars=['index'], var_name='offset').assign(type='horvath')
+
+# accs_df['std'] = accs_df.groupby('index')['value'].transform('std')
+# accs_df['normalised_value'] = accs_df.value/accs_df['std']
+# horvaths_df['std'] = horvaths_df.groupby('index')['value'].transform('std')
+# horvaths_df['normalised_value'] = horvaths_df.value/horvaths_df['std']
+
+# df = pd.concat((accs_df, horvaths_df), axis=0)
+# df = df.drop(['offset'], axis=1).rename(columns={'index': 'offset'})
+
+# #%% plot
+# ax=sns.boxplot(data=df, x='offset', 
+#                 y='normalised_value', hue='type', showfliers=False)
+
+# #%% save
+# plot.save(ax, 'A_Horvath_bias_problem', format='svg')
+# plot.save(ax, 'A_Horvath_bias_problem', format='png')
--- plots/A_jointplot_accVSage.py
+++ plots/A_jointplot_accVSage.py
@@ -1,4 +1,5 @@
-# %%  Imports
+# %%  
+# Imports
 import sys
 sys.path.append("..")   # fix to import modules from root
 from src.general_imports import *
@@ -6,9 +7,11 @@ from src.general_imports import *
 DATA_PATH = '../exports/wave3_acc.h5ad'
 amdata = ad.read_h5ad(DATA_PATH)
 
+N_BINS = 5
+
 participants = amdata.var
 
-participants['age_bin'] = pd.cut(participants.age, 10, precision=0).astype('str')
+participants['age_bin'] = pd.cut(participants.age, N_BINS, precision=0).astype('str')
 age_bins = np.sort(participants.age_bin.unique())
 
 from sklearn.linear_model import LinearRegression
@@ -16,11 +19,16 @@ from sklearn.linear_model import LinearRegression
 bin_size = participants.groupby('age_bin')['pos'].count()
 bin_centers = participants.groupby('age_bin')['age'].mean().values[:,np.newaxis]
 
+var_df = pd.DataFrame()
+for age_bin in age_bins:
+    bin = pd.DataFrame(amdata[:,amdata.var.age_bin == age_bin].X.var(axis=1).toarray(), columns=['variance'])
+    bin['bin'] = age_bin
+    var_df= pd.concat((var_df, bin), axis=0)
 
 
-
-# %%  Run for a clock
-CLOCK = 'acc'
+# %%  
+# Run for a our acceleration
+CLOCK = 'Horvath'
 
 bin_means = participants.groupby('age_bin')[CLOCK].mean().values
 bin_sds = participants.groupby('age_bin')[CLOCK].std().values
@@ -33,24 +41,21 @@ std_up = mean + 2*std
 std_down = mean - 2*std
 
 
-g = sns.JointGrid()
+g = sns.JointGrid(ylim=(-20,20))
 sns.scatterplot(data=participants, x='age', y=CLOCK, alpha=0.3, hue='age_bin', 
-                palette="rocket",  hue_order=age_bins, ax=g.ax_joint, legend=False)
+                palette=CON_PALLETE,  hue_order=age_bins, ax=g.ax_joint, legend=False)
 sns.lineplot(x=bin_centers.flatten(), color='tab:blue', label='Mean', y=mean, ax=g.ax_joint)
 sns.lineplot(x=bin_centers.flatten(), color='tab:blue', label='2*Standard deviation', y=std_up, ax=g.ax_joint)
 sns.lineplot(x=bin_centers.flatten(), color='tab:blue', y=std_down, ax=g.ax_joint)
 sns.boxplot(data=participants, y=CLOCK, x=participants.age_bin, hue_order=age_bins, 
-                ax=g.ax_marg_y, palette="rocket", showfliers=False)
-# sns.kdeplot(data=participants, y='acc', hue=participants.age_bin, hue_order=age_bins, ax=g.ax_marg_y, 
-#                 palette="rocket", legend=False)
-g.fig.suptitle(f'{CLOCK} acceleration distribution in different age bins', y=0.8)
-g.fig.subplots_adjust(top=0.8)
+                ax=g.ax_marg_y, palette=CON_PALLETE, showfliers=False)
+sns.boxplot(data=participants, y=CLOCK, x=participants.age_bin, hue_order=age_bins, 
+                ax=g.ax_joint, palette=CON_PALLETE, showfliers=False)
+sns.pointplot(data=var_df, x="bin", y="variance", ax=g.ax_marg_x)
 g.refline(y=0)
+g.fig.subplots_adjust(top=0.7)
 
-
-
-
-
+g.savefig('../results/A_jointplot_accVSage1.svg')
 
 # %%  Run for a clock
 CLOCK = 'Horvath'
