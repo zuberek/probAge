9b31233d932064876e2b54f7277403eb637bac08
Jan Dabrowski <s1768506@ed.ac.uk>
2023-09-19 12:07:42 +0100

    Add advi fit and restructure fitting function

--- src/modelling_bio.py
+++ src/modelling_bio.py
@@ -4,6 +4,7 @@ import arviz as az
 import numpy as np
 from scipy.stats import norm, beta
 from scipy.optimize import minimize
+from pymc.variational.callbacks import CheckParametersConvergence
 # from pymc.sampling_jax import sample_numpyro_nuts
 # from pymc.sampling import jax
 import pandas as pd
@@ -87,7 +88,7 @@ def linear_sites(amdata, return_MAP=False, return_trace=True, cores=CORES, show_
 def bio_site_mean(ages, eta_0, omega, p):
     return eta_0 + np.exp(-omega*ages)*(p-eta_0)
 
-def bio_sites(amdata, return_MAP=False, return_trace=True, show_progress=False, init_nuts='auto', target_accept=0.9, cores=CORES):
+def bio_sites(amdata, return_MAP=False, return_trace=True, show_progress=False, init_nuts='auto', target_accept=0.8, cores=CORES):
 
     if show_progress: print(f'Modelling {amdata.shape[0]} bio_sites')
     ages = np.broadcast_to(amdata.var.age, shape=(amdata.shape[0], amdata.shape[1])).T
@@ -117,6 +118,7 @@ def bio_sites(amdata, return_MAP=False, return_trace=True, show_progress=False, 
         mean = eta_0 + np.exp(-omega*ages)*((p-1)*eta_0 + p*eta_1)
         # mean = bio_site_mean(ages, eta_0, omega, p)
 
+
         variance = (var_term_0/N 
                 + np.exp(-omega*ages)*(var_term_1-var_term_0)/N 
                 + np.exp(-2*omega*ages)*(var_init/np.power(N,2) - var_term_1/N)
@@ -124,7 +126,7 @@ def bio_sites(amdata, return_MAP=False, return_trace=True, show_progress=False, 
 
         # Beta function parameter boundaries
         sigma = np.sqrt(variance)
-        sigma = pm.math.minimum(sigma,  pm.math.sqrt(mean * (1 - mean)) - 0.0001)
+        # sigma = pm.math.minimum(sigma,  pm.math.sqrt(mean * (1 - mean)) - 0.0001)
 
         # Define likelihood
         likelihood = pm.Beta("m-values",
@@ -150,6 +152,76 @@ def bio_sites(amdata, return_MAP=False, return_trace=True, show_progress=False, 
                     extend_inferencedata=True)
             res['trace'] = trace
     return res
+
+def bio_sites(amdata, method='MAP', show_progress=True, init_nuts='auto', target_accept=0.8, cores=CORES):
+
+    if show_progress: print(f'Computing {compute} of {amdata.shape[0]} bio_sites')
+    ages = np.broadcast_to(amdata.var.age, shape=(amdata.shape[0], amdata.shape[1])).T
+    coords = {'sites': amdata.obs.index.values,
+            'participants': amdata.var.index.values}
+
+    with pm.Model(coords=coords) as model:
+
+        # condition on maximal initial standard deviation
+        init_std_bound = 0.1
+
+        # Define priors
+        eta_0 = pm.Uniform("eta_0", lower=0, upper=1, dims='sites')
+        omega = pm.HalfNormal("omega", sigma=0.02, dims='sites')
+        p = pm.Uniform("meth_init", lower=0, upper=1, dims='sites')
+        N = pm.Uniform('system_size', lower= 1, upper=2_000, dims='sites')
+        var_init = pm.Uniform("var_init", lower=0,
+                                          upper=2*np.power(init_std_bound*N,2),
+                                          dims='sites')
+        
+        # Useful variables
+        eta_1 = 1-eta_0
+        
+        # model mean and variance
+        var_term_0 = eta_0*eta_1
+        var_term_1 = (1-p)*np.power(eta_0,2) + p*np.power(eta_1,2)
+
+        mean = eta_0 + np.exp(-omega*ages)*((p-1)*eta_0 + p*eta_1)
+        # mean = bio_site_mean(ages, eta_0, omega, p)
+
+
+        variance = (var_term_0/N 
+                + np.exp(-omega*ages)*(var_term_1-var_term_0)/N 
+                + np.exp(-2*omega*ages)*(var_init/np.power(N,2) - var_term_1/N)
+            )
+
+        # Beta function parameter boundaries
+        sigma = np.sqrt(variance)
+        # sigma = pm.math.minimum(sigma,  pm.math.sqrt(mean * (1 - mean)) - 0.0001)
+
+        # Define likelihood
+        likelihood = pm.Beta("m-values",
+            mu = mean,
+            sigma = sigma,
+            dims=("participants", "sites"),
+            observed = amdata.X.T)
+
+
+        res = {}
+        if method=='map':
+            return pm.find_MAP(progressbar=show_progress)
+
+        if method=='advi':
+            mean_field = pm.fit(method='advi', n=100_000, callbacks=[CheckParametersConvergence()],  progressbar=show_progress)
+            return mean_field.sample(1_000)
+
+        if method=='nuts':
+            trace = pm.sample(1000, tune=1000, init=init_nuts,
+                                     chains=CHAINS, cores=cores,
+                                     progressbar=show_progress,
+                                     target_accept=target_accept, 
+                                     nuts_sampler='nutpie')
+
+            pm.compute_log_likelihood(trace, progressbar=False)
+            ppc = pm.sample_posterior_predictive(trace,
+                    extend_inferencedata=True)
+            return trace
+
 
 def bio_model(amdata):
 
@@ -201,7 +273,7 @@ def bio_model(amdata):
 
     return model
 
-def bio_model_fit(amdata_chunk, model, return_MAP=False, return_trace=True, show_progress=False, init_nuts='auto', target_accept=0.8, cores=CORES):
+def bio_model_fit(model, amdata_chunk, return_MAP=False, return_trace=True, show_progress=False, init_nuts='auto', target_accept=0.8, cores=CORES):
 
     with model:
 
@@ -231,6 +303,7 @@ def person_model(amdata,
                          show_progress=False,
                          init_nuts='auto',
                          cores=CORES,
+                         nuts_sampler='pymc',
                          map_method='L-BFGS-B'):
 
     if show_progress: print(f'Modelling {amdata.shape[1]} participants')
@@ -251,6 +324,8 @@ def person_model(amdata,
 
     # Define Pymc model
     with pm.Model(coords=coords) as model:
+
+        
         
         # Define model variables
         acc = pm.Uniform('acc', lower=0.33, upper=3, dims='part')
@@ -275,13 +350,22 @@ def person_model(amdata,
         
         # Beta function parameter boundaries
         sigma = np.sqrt(variance)
-        sigma = pm.math.minimum(sigma,  pm.math.sqrt(mean * (1 - mean)) - 0.0001)
+        # sigma = pm.math.minimum(sigma,  pm.math.sqrt(mean * (1 - mean)) - 0.0001)
+
 
 
         # Define likelihood
-        obs = pm.Beta("obs",
+        # y_latent = pm.Normal.dist(mu=mean, sigma=sigma)
+        # obs = pm.Censored("obs",
+        #                   y_latent,
+        #                   lower=0, upper=1,
+        #                   observed=amdata.X)
+        
+        # # Define likelihood
+        obs = pm.Normal("obs",
                             mu=mean,
                             sigma = sigma, 
+                            # lower=0, upper=1,
                             dims=("site", "part"), 
                             observed=amdata.X)
 
@@ -294,8 +378,94 @@ def person_model(amdata,
 
         if return_trace:
             res['trace'] = pm.sample(1000, tune=1000, init=init_nuts,
-                                    chains=CHAINS, cores=cores,
+                                    chains=CHAINS, cores=cores, nuts_sampler=nuts_sampler,
                                     progressbar=show_progress)
+
+    return res    
+
+def person_model(amdata,
+                         method='map',
+                         show_progress = True,
+                         init_nuts='auto',
+                         cores=CORES,
+                         target_accept=0.9,
+                         nuts_sampler='pymc',
+                         map_method='L-BFGS-B'):
+
+    
+    # if 'age' in amdata.obs.columns:
+    #     print('got here')
+    #     amdata = amdata.copy().T
+
+    if show_progress: print(f'Modelling {amdata.shape[1]} participants')
+
+    # The data has two dimensions: participant and CpG site
+    coords = {"site": amdata.obs.index, "part": amdata.var.index}
+
+    # # create a numpy array of the participants ages
+    # # array of ages needs to be broadcasted into a matrix array for each CpG site
+    omega = np.broadcast_to(amdata.obs.omega, shape=(amdata.shape[1], amdata.shape[0])).T
+    eta_0 = np.broadcast_to(amdata.obs.eta_0, shape=(amdata.shape[1], amdata.shape[0])).T
+    p = np.broadcast_to(amdata.obs.meth_init, shape=(amdata.shape[1], amdata.shape[0])).T
+    var_init = np.broadcast_to(amdata.obs.var_init, shape=(amdata.shape[1], amdata.shape[0])).T
+    N = np.broadcast_to(amdata.obs.system_size, shape=(amdata.shape[1], amdata.shape[0])).T
+
+    age = amdata.var.age.values
+
+    # Define Pymc model
+    with pm.Model(coords=coords) as model:
+
+        # Define model variables
+        acc = pm.Uniform('acc', lower=0.33, upper=3, dims='part')
+        bias = pm.Normal('bias', mu=0, sigma=0.1, dims='part')
+
+        # Useful variables
+        omega = acc*omega
+        eta_1 = 1-eta_0
+        
+        # model mean and variance
+        var_term_0 = eta_0*eta_1
+        var_term_1 = (1-p)*np.power(eta_0,2) + p*np.power(eta_1,2)
+
+
+        # mean = eta_0 + np.exp(-omega*age)*((p-1)*eta_0 + p*eta_1)
+        mean = eta_0 + np.exp(-omega*age)*((p-1)*eta_0 + p*eta_1) + bias
+
+        variance = (var_term_0/N 
+                + np.exp(-omega*age)*(var_term_1-var_term_0)/N 
+                + np.exp(-2*omega*age)*(var_init/np.power(N,2) - var_term_1/N)
+            )
+        
+        # Beta function parameter boundaries
+        sigma = np.sqrt(variance)
+        # sigma = pm.math.minimum(sigma,  pm.math.sqrt(mean * (1 - mean)) - 0.0001)
+
+        obs = pm.Beta("obs",
+                            mu=mean,
+                            sigma = sigma, 
+                            # lower=0, upper=1,
+                            dims=("site", "part"), 
+                            observed=amdata.X)
+
+        if method=='map':
+            return pm.find_MAP(progressbar=show_progress)
+
+        if method=='advi':
+            mean_field = pm.fit(method='advi', n=100_000, callbacks=[CheckParametersConvergence()],  progressbar=show_progress)
+            return mean_field.sample(10_000)
+
+        if method=='nuts':
+            trace = pm.sample(1000, tune=1000, init=init_nuts,
+                                     chains=CHAINS, cores=cores,
+                                     progressbar=show_progress,
+                                     target_accept=target_accept, 
+                                     nuts_sampler='nutpie')
+
+            pm.compute_log_likelihood(trace, progressbar=False)
+            ppc = pm.sample_posterior_predictive(trace,
+                    extend_inferencedata=True)
+            return trace
+
 
     return res    
 
@@ -385,6 +555,7 @@ def bio_fit_post(results, amdata):
     return fits
 
 
+
 def make_clean_trace(trace):
     delattr(trace, 'sample_stats')
     delattr(trace, 'observed_data')
@@ -470,15 +641,19 @@ def get_conf_int(amdata, t=np.linspace(0,100, 1_00)):
 
     return mean, low_conf, upper_conf
 
-def bio_model_plot (amdata, alpha=1, fits=None, ax=None, hue='tab:grey'):
+def bio_model_plot (amdata, alpha=1, fits=None, ax=None, hue='grey'):
     """Plot the evolution of site predicted by bio_model"""
     xlim=(0,100)
     t = np.linspace(xlim[0],xlim[1], 1_000)
 
     mean, variance = bio_model_stats(amdata, t)
 
-    a = ((1-mean)/variance - 1/mean)*np.power(mean,2)
-    b = a*(1/mean - 1)
+    k = (mean*(1-mean)/variance)-1
+    a = mean*k
+    b = (1-mean)*k
+
+    # a = ((1-mean)/variance - 1/mean)*np.power(mean,2)
+    # b = a*(1/mean - 1)
 
     conf_int = np.array(beta.interval(0.95, a, b))
     low_conf = conf_int[0]
@@ -489,7 +664,7 @@ def bio_model_plot (amdata, alpha=1, fits=None, ax=None, hue='tab:grey'):
 
     sns.scatterplot(x=amdata.var.age,
                     y=amdata.X.flatten(),
-                    hue=hue,
+                    # hue=hue,
                     # label='data',
                     alpha=alpha, ax=ax
                     )
@@ -546,7 +721,7 @@ def is_saturating_vect(amdata):
     """Check if site is saturating at birth or 100yo.
     We use the predicted 95% CI from the bio_model"""
 
-    t = np.array([0, 100])
+    t = np.array([99, 100])
     mean, variance = bio_model_stats_vect(amdata, t)
     
     a = ((1-mean)/variance - 1/mean)*np.power(mean,2)
