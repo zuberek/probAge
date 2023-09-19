5d926c7c88f3dd8cc90f0bcbf514766fde699726
Jan Dabrowski <s1768506@ed.ac.uk>
2023-09-19 12:08:08 +0100

    Update paths

--- src/paths.py
+++ src/paths.py
@@ -3,14 +3,16 @@ DATASET = 'Nelly'
 
 # Raw
 DATA_RAW = '../../methylation/ewas/age_methylation_v1.txt'
+DATA_RAW = '../../methylation/wave4/wave4.h5ad'
 # DATA_RAW = '../data/Patients_betas_reduced_ready-40.csv'
 # DATA_PATIENTS = '../data/Annotation_paediatric_40.csv'
 
 # Processed
 DATA_PROCESSED_DIR = '../exports'
 # DATA_PROCESSED = '../exports/wave1_meta.h5ad'
-# DATA_PROCESSED = '../exports/wave1_meta.h5ad'
-DATA_PROCESSED = '../exports/ewasKNN.h5ad'
+# DATA_PROCESSED = '../exports/wave3_meta.h5ad'
+DATA_PROCESSED = '../../methylation/wave4/wave4_meta.h5ad'
+# DATA_PROCESSED = '../exports/ewasKNN.h5ad'
 # DATA_PROCESSED = '../../methylation/ewas/age_methylation_v1.h5ad'
 # DATA_PROCESSED = '../exports/Nelly_raw.h5ad'
 # DATA_PROCESSED = f'../exports/{DATASET}_meta.h5ad'
@@ -18,11 +20,15 @@ DATA_PROCESSED = '../exports/ewasKNN.h5ad'
 # Fitted
 # DATA_FITTED = '../exports/Nelly_fitted.h5ad'
 # DATA_FITTED = '../exports/wave1_fitted.h5ad'
-DATA_FITTED = '../exports/ewasKNN_fitted.h5ad'
+# DATA_FITTED = '../exports/ewasKNN_fitted.h5ad'
+# DATA_FITTED = '../exports/ewasKNN_fitted_ADVI.h5ad'
 # DATA_FITTED = '../exports/wave3_linear.h5ad'
+# DATA_FITTED = '../exports/wave3_all_fitted.h5ad'
+DATA_FITTED = '../../methylation/wave4/wave4_fitted_ADVI.h5ad'
 # DATA_FITTED = f'../exports/{DATASET}_fitted.h5ad'
 
 # Other
-DATA_REFERENCE = '../exports/ewas_fitted.h5ad'
-DATA_REFERENCE = '../exports/ewasKNN_fitted.h5ad'
+# DATA_REFERENCE = '../exports/ewas_fitted.h5ad'
+DATA_REFERENCE = '../../methylation/wave4/wave4_fitted.h5ad'
+# DATA_REFERENCE = '../exports/ewasKNN2_fitted.h5ad'
 # DATA_REFERENCE = '../exports/wave3_MAP_acc.h5ad'
