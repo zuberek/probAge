5a5e549da58724a5518fa9e546fd9b9428901969
Jan Dabrowski <s1768506@ed.ac.uk>
2023-09-19 12:08:59 +0100

    Add spearman computation to preprocessing

--- scripts_clean/make_anndata.py
+++ scripts_clean/make_anndata.py
@@ -9,6 +9,8 @@ sys.path.append("..")   # fix to import modules from root
 from src.general_imports import *
 from src import paths
 import src.preprocess_func  as preprocess_func
+from sklearn.feature_selection import r_regression
+import scipy.stats
 
 # %%
 # LOAD
@@ -29,6 +31,15 @@ amdata = ad.AnnData(X= methylation.values,
 amdata = preprocess_func.drop_nans(amdata)
 amdata.X = np.where(amdata.X == 0, 0.00001, amdata.X)
 amdata.X = np.where(amdata.X == 1, 0.99999, amdata.X)
+
+def spearman_r_loop(site_idx):
+    spr = stats.spearmanr(amdata[site_idx].X.flatten(), amdata.var.age)
+    return spr.statistic
+
+with Pool(15) as pool:
+    result = list(tqdm(pool.imap(spearman_r_loop, amdata.obs.index), total=amdata.shape[0]))
+
+stats.spearmanr(amdata.X, ages, axis=None)
 
 # %%
 # SAVE
