6951f8cf7af73861b606d75069837a25a73819f6
Jan Dabrowski <1768506@ed.ac.uk>
2023-10-18 12:20:04 +0100

    Add new data references

--- scripts_clean/external.py
+++ scripts_clean/external.py
@@ -17,21 +17,23 @@ from src import modelling_bio
 # %%
 # LOAD
 
-amdata = ad.read_h5ad(paths.DATA_PROCESSED, backed='r')
-amdata_ref = ad.read_h5ad(paths.DATA_REFERENCE)
-amdata_ref = amdata_ref[~amdata_ref.obs.saturating]
+external = 'wave4'
+reference = 'EWAS'
+
+amdata = ad.read_h5ad(f'../exports/{external}_fitted.h5ad', backed='r')
+participants = pd.read_csv(f'../exports/{external}_participants.csv')
+amdata_ref = ad.read_h5ad(f'../exports/{reference}_fitted.h5ad', backed='r')
+sites_ref = amdata_ref[~amdata_ref.obs.saturating].obs
 
 # Load intersection of sites in new dataset
 params = modelling_bio.get_site_params()
 
 # intersection = site_info.index.intersection(amdata.obs.index)
-intersection = amdata_ref.obs.index.intersection(amdata.obs.index)
-amdata = amdata[intersection].to_memory()
+intersection = sites_ref.index.intersection(amdata.obs.index)
+intersection = sites_ref.loc[intersection].sort_values('spr2').tail(500).index
 
-amdata.obs[params + ['r2', 'spr2']] = amdata_ref.obs[params + ['r2', 'spr2']]
-amdata = amdata[amdata.obs.sort_values('spr2', ascending=False).index]
-amdata = amdata[amdata.obs.sort_values('spr2').tail(500).index]
-amdata = amdata.copy()
+amdata = amdata[intersection].to_memory()
+amdata.obs[params + ['r2', 'spr2']] = sites_ref[params + ['r2', 'spr2']]
 
 
 # %% #################
@@ -65,7 +67,7 @@ amdata.obs.meth_init  = amdata.obs.meth_init + amdata.obs.offset
 # accs = np.empty((len(n_sites_grid), amdata.shape[1]))
 # biases = np.empty((len(n_sites_grid), amdata.shape[1]))
 # for i, n_sites in enumerate(tqdm(n_sites_grid)):
-#     map = modelling_bio.person_model(amdata[:n_sites], return_MAP=True, return_trace=False)['map']
+#     map = modelling_bio.person_model(amdata[:n_sites], method='map', show_progress=True)
 #     accs[i] = map['acc']
 #     biases[i] = map['bias']    
 # accs = pd.DataFrame(accs, index=n_sites_grid, columns=amdata.var.index).T
@@ -82,26 +84,9 @@ amdata.obs.meth_init  = amdata.obs.meth_init + amdata.obs.offset
 # %% ##################
 # PERSON MODELLING  
 
-ab_maps = modelling_bio.person_model(amdata, return_MAP=True, return_trace=False, show_progress=True)['map']
+ab_maps = modelling_bio.person_model(amdata, method='map', show_progress=True)
 
-amdata.var['acc_wave4'] = ab_maps['acc']
-amdata.var['bias_wave4'] = ab_maps['bias']
+participants[f'acc_{reference}'] = ab_maps['acc']
+participants[f'bias_{reference}'] = ab_maps['bias']
 
-amdata.var.to_csv('wave3_participants.csv')
-
-amdata.var['acc_ewas532'] = ab_maps['acc']
-amdata.var['bias_ewas532'] = ab_maps['bias']
-
-amdata.var.to_csv('lbc_var.csv')
-
-ab = ab.rename(columns={'acc':'acc_ewas532', 'bias': 'bias_ewas532'})
-ab['acc_gs250'] = ab_maps['acc']
-ab['bias_gs250'] = ab_maps['bias']
-
-sns.scatterplot(data=ab, x='acc_gs250', y='acc_ewas532')
-ab=ab.set_index('Unnamed: 0')
-ab.index.name = None
-ab.to_csv('ab.csv')
-ab.columns
-
-amdata.write_h5ad(paths.DATA_FITTED)
+participants.to_csv(f'../exports/{external}_participants.csv')
