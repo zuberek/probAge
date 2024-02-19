a99e67773e9e25d4706e7b7c6c33051c0244cd11
zuberek <jan.dobrow@gmail.com>
2024-02-19 10:53:29 +0000

    Finish figure 3,4 and 5

--- scripts_figures/3C_jointplot_acc_bias_distribution.py
+++ scripts_figures/3C_jointplot_acc_bias_distribution.py
@@ -13,17 +13,21 @@ participants = amdata.var
 participants['data'] = 'Every participant'
 
 #%%
+plot.fonts(8)
 g = sns.JointGrid()
-sns.scatterplot(data=amdata.var, alpha=0.5, x='acc', y='bias', color='tab:grey', ax=g.ax_joint)
-sns.kdeplot(data=amdata.var, x='acc', y='bias', color='black',  ax=g.ax_joint)
+sns.scatterplot(data=amdata.var, alpha=0.3, x='acc', y='bias', color='tab:grey', ax=g.ax_joint)
+sns.kdeplot(data=amdata.var, levels=5, x='acc', y='bias', color='black',  ax=g.ax_joint)
 sns.kdeplot(data=amdata.var, x='acc', color='black', ax=g.ax_marg_x, legend=False)
 sns.kdeplot(data=amdata.var, y='bias', color='black', ax=g.ax_marg_y, legend=False)
 g.refline(x=0,y=0)
 
-g.fig.set_figwidth(6)
-g.fig.set_figheight(4)
+g.ax_joint.set_ylabel('Bias (global beta)')
+g.ax_joint.set_xlabel('Acceleration (beta/year)')
+
+g.figure.set_size_inches((plot.cm2inch(9.5),plot.cm2inch(6)))
+g.figure.tight_layout()
 
 # %% saving
-g.savefig(f'{paths.FIGURES_DIR}/3C_jointplot_accbias_distribution.svg')
-g.savefig(f'{paths.FIGURES_DIR}/3C_jointplot_accbias_distribution.png')
+g.savefig(f'{paths.FIGURES_DIR}/fig3/3C_jointplot_accbias_distribution.svg')
+g.savefig(f'{paths.FIGURES_DIR}/fig3/3C_jointplot_accbias_distribution.png')
 #%%
