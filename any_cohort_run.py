e3d93c4e5efe987f558d24d0e54f4d8f668e38b1
elc08 <lactovit@hotmail.com>
2024-12-18 16:23:38 +0100

    update control in any_cohort

--- any_cohort_run.py
+++ any_cohort_run.py
@@ -1,6 +1,24 @@
- # fix to import modules from root
-from src.general_imports import *
+"""
+ProbAge Inference Script
+=======================
 
+This script performs epigenetic age acceleration inference on methylation data using the ProbAge model.
+It handles data loading, batch correction, and inference of acceleration and bias parameters for each participant.
+
+Required input files:
+    - Methylation data CSV (samples as columns, CpG sites as rows)
+    - Metadata CSV (sample information including age and control status)
+        - one can use the whole cohort as control status if there are no group distinction in the samples.
+    - Pre-trained model site information (provided in streamlit/wave3_sites.csv)
+
+Output files:
+    - H5AD file containing both participant and CpG-level information
+    - CSV file with updated metadata including
+        - acceleration and bias parameters
+        - log-likelihood and quality control measures
+"""
+
+from src.general_imports import *
 from src import modelling_bio_beta as modelling
 from src import batch_correction as bc
 
@@ -9,7 +27,7 @@ from src import batch_correction as bc
 # Load data into anndata format
 path_to_data = 'data/wave4_data.csv'
 path_to_metadata = 'data/wave4_metadata.csv'
-h5ad_export_path = ' data/fitted_cohort.h5ad'
+h5ad_export_path = 'data/fitted_cohort.h5ad'
 pandas_export_path = 'data/person_fit.csv'
 
 # set number of cores used for inference (as low as 1)
@@ -18,6 +36,7 @@ N_CORES = 32
 
 data_df = pd.read_csv(path_to_data, index_col=0)
 metadata_df = pd.read_csv(path_to_metadata, index_col=0)
+# metadata_df['control'] = np.random.choice([True, False], size=len(metadata_df))  
 
 # Create Anndata making sure samples are overlapping
 sample_list = set(data_df.columns)
@@ -32,10 +51,10 @@ adata.obs = sites_ref.loc[list(adata.obs.index)]
 # Load intersection of sites in new dataset
 params = list(modelling.SITE_PARAMETERS.values())
 print('Inferring site offsets... ')
-offsets = bc.site_offsets(adata, show_progress=True)['offset']
+offsets = bc.site_offsets(adata[:, adata.var.control == True], show_progress=True)['offset']
 # print('Inferring site offsets ✅')
 
-    
+ 
 adata.obs['offset'] = offsets.astype('float64')
 adata.obs.eta_0 = adata.obs.eta_0 + adata.obs.offset
 adata.obs.meth_init  = adata.obs.meth_init + adata.obs.offset
@@ -76,11 +95,12 @@ ab_ll = modelling.person_model_ll(adata)
 adata.var['ll'] = ab_ll
 adata.var['qc'] = modelling.get_person_fit_quality(ab_ll)
 
+# export h5ad file with both participant and CpG-level information
 adata.write_h5ad(h5ad_export_path)
 
+# export updated metadata file
 metadata_df['acc'] = adata.var['acc']
 metadata_df['bias'] = adata.var['bias']
 metadata_df['ll'] = adata.var['ll']
 metadata_df['qc'] = adata.var['qc']
-
 metadata_df.to_csv(pandas_export_path)
