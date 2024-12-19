8b7183ae22fa33fc63f33430399bfbb48294154e
elc08 <lactovit@hotmail.com>
2024-12-19 12:05:43 +0100

    update retrain external cohort

--- any_cohort_run.py
+++ any_cohort_run.py
@@ -21,22 +21,32 @@ Output files:
 from src.general_imports import *
 from src import modelling_bio_beta as modelling
 from src import batch_correction as bc
+from sklearn.feature_selection import r_regression
 
-# adata = ad.read_h5ad('data/wave4_meta_3000.h5ad')
 
-# Load data into anndata format
+###### GLOBAL PARAMETERS TO SET
+# Set paths for loading and dumping data
 path_to_data = 'data/wave4_data.csv'
 path_to_metadata = 'data/wave4_metadata.csv'
 h5ad_export_path = 'data/fitted_cohort.h5ad'
 pandas_export_path = 'data/person_fit.csv'
 
+# set retrain option to choose wether to refit 
+# the dynamics observed in sites or not
+retrain = True
+
 # set number of cores used for inference (as low as 1)
 MULTIPROCESSING = True
 N_CORES = 32
 
+# set wether we want to use beta or normal function for model fit
+beta_fit = True
+
+###### START OF INFERENCE
+# Load data into anndata format
 data_df = pd.read_csv(path_to_data, index_col=0)
 metadata_df = pd.read_csv(path_to_metadata, index_col=0)
-# metadata_df['control'] = np.random.choice([True, False], size=len(metadata_df))  
+metadata_df['control'] = np.random.choice([True, False], size=len(metadata_df))  
 
 # Create Anndata making sure samples are overlapping
 sample_list = set(data_df.columns)
@@ -48,16 +58,36 @@ sites_ref = pd.read_csv('streamlit/wave3_sites.csv', index_col=0)
 adata = adata[adata.obs.index.isin(sites_ref.index)].copy()
 adata.obs = sites_ref.loc[list(adata.obs.index)]
 
-# Load intersection of sites in new dataset
-params = list(modelling.SITE_PARAMETERS.values())
-print('Inferring site offsets... ')
-offsets = bc.site_offsets(adata[:, adata.var.control == True], show_progress=True)['offset']
-# print('Inferring site offsets ✅')
+#
+if retrain is True:
+    control_data = adata[:, adata.var.control==True].copy()
+    adata_chunks = modelling.make_chunks(control_data, chunk_size=15)
 
- 
-adata.obs['offset'] = offsets.astype('float64')
-adata.obs.eta_0 = adata.obs.eta_0 + adata.obs.offset
-adata.obs.meth_init  = adata.obs.meth_init + adata.obs.offset
+    # single process
+    if not MULTIPROCESSING:
+        map_chunks = [modelling.site_MAP(chunk) for chunk in tqdm(adata_chunks)]
+
+    # multiprocessing
+    if MULTIPROCESSING:
+        with Pool(N_CORES) as p:
+            map_chunks = list(tqdm(p.imap(modelling.site_MAP, adata_chunks), total=len(adata_chunks)))
+
+    # store results in original adata
+    for param in modelling.SITE_PARAMETERS.values():
+        param_data = np.concatenate([map[param] for map in map_chunks])
+        adata.obs[param] = param_data
+    adata = modelling.get_saturation_inplace(adata)
+else:
+    # Load intersection of sites in new dataset
+    params = list(modelling.SITE_PARAMETERS.values())
+    print('Inferring site offsets for batch correction...')
+    offsets = bc.site_offsets(adata[:, adata.var.control == True], show_progress=True)['offset']
+    # print('Inferring site offsets ✅')
+
+
+    adata.obs['offset'] = offsets.astype('float64')
+    adata.obs.eta_0 = adata.obs.eta_0 + adata.obs.offset
+    adata.obs.meth_init  = adata.obs.meth_init + adata.obs.offset
 
 # check that sites are properly fitted
 site_index = -1
@@ -65,13 +95,14 @@ modelling.bio_model_plot(adata[site_index])
 
 print('Inferring participants accelerations and biases...  ')
 
+adata = adata = adata[:, :2000].copy()
 adata_chunks = modelling.make_chunks(adata.T, chunk_size=10)
 adata_chunks = [chunk.T for chunk in adata_chunks]
 
 partial_func = partial(modelling.person_model,
                        method='map',
                        progressbar=False,
-                       map_method='L-BFGS-B', beta=True)
+                       map_method='L-BFGS-B', beta=beta_fit)
 
 if MULTIPROCESSING:
     with Pool(N_CORES) as p:
@@ -99,8 +130,4 @@ adata.var['qc'] = modelling.get_person_fit_quality(ab_ll)
 adata.write_h5ad(h5ad_export_path)
 
 # export updated metadata file
-metadata_df['acc'] = adata.var['acc']
-metadata_df['bias'] = adata.var['bias']
-metadata_df['ll'] = adata.var['ll']
-metadata_df['qc'] = adata.var['qc']
-metadata_df.to_csv(pandas_export_path)
+adata.var.to_csv(pandas_export_path)
